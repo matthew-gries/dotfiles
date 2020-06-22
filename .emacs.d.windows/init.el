0c4cfc2458cb94527a73d63ee7e9c8b18780919e
Matthew Gries <46942482+matthew-gries@users.noreply.github.com>
2020-06-22 19:03:07 +0000

    Add files via upload

---
+++ .emacs.d.windows/init.el
@@ -0,0 +1,74 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d1af5ef9b24d25f50f00d455bd51c1d586ede1949c5d2863bef763c60ddf703a" default)))
+ '(package-selected-packages
+   (quote
+    (flycheck-rust evil-magit counsel-projectile magit lsp-mode rust-mode evil-collection avy flycheck company all-the-icons neotree dashboard airline-themes powerline counsel projectile which-key atom-one-dark-theme evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq evil-want-integration t)
+(setq evil-want-keybinding nil)
+(setq evil-want-C-u-scroll t)
+
+(require 'evil)
+(when (require 'evil-collection nil t)
+  (evil-collection-init))
+(evil-mode 1)
+(evil-set-initial-state 'neotree-mode 'emacs)
+(evil-set-initial-state 'dashboard-mode 'emacs)
+(evil-set-initial-state 'dired-mode 'emacs)
+(evil-set-initial-state 'shell-mode 'emacs)
+
+(setq evil-magit-state 'normal)
+(setq evil-magit-use-y-for-yank nil)
+(require 'evil-magit)
+
+(require 'flycheck)
+
+;;(require 'powerline)
+;;(require 'airline-themes)
+(require 'page-break-lines)
+(add-to-list 'load-path "~/.emacs.d/elpa/page-break-lines/page-break-lines-20200305.244.el")
+(require 'dashboard)
+(require 'neotree)
+(require 'all-the-icons)
+(require 'rust-mode)
+(require 'company-lsp)
+(push 'company-lsp company-backends)
+(require 'lsp-mode)
+(add-hook 'c-mode-hook #'lsp)
+(add-hook 'rust-mode-hook #'lsp)
+;;(add-hook 'python-mode-hook 'jedi:setup)
+;;(setq jedi:complete-on-dot t)
+;;(setenv "WORKON_HOME" "~/Anaconda3/envs")
+;;(pyvenv-mode 1)
+
+(counsel-projectile-mode)
+
+(load "~/.emacs.d/config.el")
