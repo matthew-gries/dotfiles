7a669eaaebf938cb7706a410b1af3f8266debac7
Matthew Gries <46942482+matthew-gries@users.noreply.github.com>
2020-08-11 10:37:56 -0400

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,7 +25,7 @@ There are two things you can do about this warning:
     ("d1af5ef9b24d25f50f00d455bd51c1d586ede1949c5d2863bef763c60ddf703a" default)))
  '(package-selected-packages
    (quote
-    (company-irony irony-eldoc irony auctex flycheck-rust evil-magit pyvenv jedi eglot counsel company-lsp magit rust-mode evil-collection avy flycheck company all-the-icons neotree dashboard airline-themes powerline projectile which-key atom-one-dark-theme evil))))
+    (yasnippet use-package company-irony irony-eldoc irony auctex flycheck-rust evil-magit pyvenv jedi eglot counsel company-lsp magit rust-mode evil-collection avy flycheck company all-the-icons neotree dashboard airline-themes powerline projectile which-key atom-one-dark-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,76 +33,199 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  )
 
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
 ;; Evil mode
-(setq evil-want-integration t)
-(setq evil-want-keybinding nil)
-(setq evil-want-C-u-scroll t)
 
-(require 'evil)
-(when (require 'evil-collection nil t)
-  (evil-collection-init))
-(evil-mode 1)
-(evil-set-initial-state 'neotree-mode 'emacs)
-(evil-set-initial-state 'dashboard-mode 'emacs)
-(evil-set-initial-state 'dired-mode 'emacs)
-(evil-set-initial-state 'shell-mode 'emacs)
+(use-package evil
+  :init
+   (setq evil-want-integration t)
+   (setq evil-want-keybinding nil)
+   (setq evil-want-C-u-scroll t)
+   :config
+   (when (require 'evil-collection nil t)
+   (evil-collection-init))
+   (evil-mode 1)
+   (evil-set-initial-state 'neotree-mode 'emacs)
+   (evil-set-initial-state 'dashboard-mode 'emacs)
+   (evil-set-initial-state 'dired-mode 'emacs)
+   (evil-set-initial-state 'shell-mode 'emacs))
 
-(setq evil-magit-state 'normal)
-(setq evil-magit-use-y-for-yank nil)
-(require 'evil-magit)
+(use-package evil-magit
+  :init
+  (setq evil-magit-state 'normal)
+  (setq evil-magit-use-y-for-yank nil))
 
 ;; General quality of life extensions
-(which-key-mode)
-(projectile-mode +1)
-(ivy-mode 1)
-(setq projectile-completion-system 'ivy)
-(require 'powerline)
-(require 'airline-themes)
-(require 'page-break-lines)
-(turn-on-page-break-lines-mode)
-(add-to-list 'load-path "~/.emacs.d/elpa/page-break-lines/page-break-lines-20200305.244.el")
-(require 'dashboard)
-(dashboard-setup-startup-hook)
-(require 'neotree)
-(require 'all-the-icons)
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package ivy
+  :config
+  (ivy-mode 1))
+
+(use-package projectile
+  :init
+  (setq projectile-completion-system 'ivy)
+  :config
+  (projectile-mode +1)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+  
+(use-package airline-themes)
+(use-package powerline
+  :init
+  (load-theme 'airline-gruvbox-dark t))
+
+(use-package page-break-lines
+  :config
+  (page-break-lines-mode))
+
+(use-package all-the-icons)
+
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "Uh oh, stinky...")
+  (setq dashboard-items '((recents  . 5)
+                          (bookmarks . 5)
+                          (projects . 5)
+                          (agenda . 5)
+                          (registers . 5)))
+  (setq dashboard-startup-banner 3)
+  (setq dashboard-center-content t))
+
+(defun neotree-evil-hook ()
+  ;;; Neotree evil settings
+  (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+  (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+  (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+  (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+  (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+  (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+  (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+  (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+  (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle))
+
+(use-package neotree
+  :hook
+  (neotree-mode . neotree-evil-hook)
+  :bind
+  ([f8] . neotree-toggle)
+  :config
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
+(use-package avy
+  :bind
+  ("C-:" . avy-goto-char)
+  ("C-'" . avy-goto-char-2)
+  ("M-g f" . avy-goto-line)
+  ("M-g w" . avy-goto-word-1)
+  ("M-g e" . avy-goto-word-0))
+
+;; flycheck
+(use-package flycheck
+  :hook
+  (after-init . global-flycheck-mode))
 
 ;; code completion engine
-(require 'company)
-(setq company-idle-delay nil)
-(setq company-minimum-prefix-length 3)
-(setq company-show-numbers t)
-(setq company-tooltip-limit 20)
-(setq company-dabbrev-downcase nil)
-(add-hook 'after-init-hook 'global-company-mode)
-;;(global-company-mode t)
-(require 'company-lsp)
-(setq compnay-lsp-enable-snippet t)
-(push 'company-lsp company-backends)
-(require 'lsp-mode)
-(require 'rust-mode)
-;;(add-hook 'c-mode-hook #'lsp)
-;;(add-hook 'c++-mode-hook #'lsp)
-(add-hook 'rust-mode-hook #'lsp)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+(use-package company
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-idle-delay nil)
+  (setq company-minimum-prefix-length 3)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 20)
+  :bind
+  ("C-;" . company-complete-common))
+
+(use-package company-lsp
+  :config
+  (push 'company-lsp company-backends))
+
+(use-package lsp-mode)
+
+(use-package rust-mode
+  :hook
+  (rust-mode . lsp)
+  (flycheck-mode . flycheck-rust-setup))
+
+(use-package jedi
+  :hook
+  (python-mode . jedi:setup)
+  :config
+  (setq jedi:complete-on-dot t))
 
 ; irony mode
-(require 'company-irony)
-(add-to-list 'company-backends 'company-irony)
-(require 'irony)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
+(use-package irony
+  :hook
+  (c++-mode . irony-mode)
+  (c-mode . irony-mode))
+
 (defun my-irony-mode-hook ()
   (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
+      'irony-completion-at-point-async)
   (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
+      'irony-completion-at-point-async))
+
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-(require 'irony-eldoc)
-(add-hook 'irony-mode-hook #'irony-eldoc)
+
+(use-package irony-eldoc
+  :hook
+  (irony-mode irony-eldoc))
+
+(use-package company-irony
+  :config
+  (add-to-list 'company-backends 'company-irony))
 
 (setenv "WORKON_HOME" "~/anaconda3/envs")
 (pyvenv-mode 1)
 
-(load "~/.emacs.d/config.el")
+;; themes, fonts, general editor settings
+(set-frame-font "Source Code Pro 12" nil t)
+(load-theme 'gruvbox t)
+;;(menu-bar-mode 0)
+(tool-bar-mode 0)
+(set-scroll-bar-mode nil)
+(toggle-truncate-lines)
+(global-display-line-numbers-mode)
+(electric-pair-mode 1)
+(setq make-backup-files nil)
+
+;; custom keybindings
+
+(defun my-indent-region (N)
+  (interactive "p")
+  (if (use-region-p)
+      (progn (indent-rigidly (region-beginning) (region-end) (* N 4))
+             (setq deactivate-mark nil))
+    (self-insert-command N)))
+
+(defun my-unindent-region (N)
+  (interactive "p")
+  (if (use-region-p)
+      (progn (indent-rigidly (region-beginning) (region-end) (* N -4))
+             (setq deactivate-mark nil))
+    (self-insert-command N)))
+
+(global-set-key (kbd "C->") 'my-indent-region)
+(global-set-key (kbd "C-<") 'my-unindent-region)
+
+;; language mode hooks and settings
+(defun my-c-mode-hook ()
+  (setq c-default-style "linux")
+  (setq c-basic-offset 4))
+
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c++-mode-hook 'my-c-mode-hook)
+
+(setq c-default-style "linux")
+(setq c-basic-offset 4)
+
