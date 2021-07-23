3c879f07d6d4984b31d571bbfe77e22123fd956d
Matthew Gries <46942482+matthew-gries@users.noreply.github.com>
2021-07-23 17:39:58 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,3 @@
-function sudo {
-	Start-Process @args -verb runas
-}
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
