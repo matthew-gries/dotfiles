21c5d7e7c46faf638c03b4e2f72325aa817ce71f
Matthew Gries <46942482+matthew-gries@users.noreply.github.com>
2025-11-14 16:36:35 -0500

    Configure indentation settings for Neovim
    Set tab and shift width to 4 spaces and enable expandtab.

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -26,6 +26,11 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Use 4 spaces for indentation
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Save undo history
 vim.o.undofile = true
 
